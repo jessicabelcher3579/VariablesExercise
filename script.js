8faff2979d4acb9c3480c7ddbd1de1b39606746a
“jessicabelcher3579” <jessicabelcher3579@gmail.com>
2022-03-02 10:04:01 -0500

    Added VariablesExercise Folder

---
+++ script.js
@@ -0,0 +1,42 @@
+// 1
+// const
+
+// 2
+let num = 20;
+console.log(num);
+
+// 3
+num = 15;
+console.log(num);
+
+// 4
+num += 7;
+console.log(num);
+
+// 5
+num--;
+console.log(num);
+
+// 6
+num = "This is not a number";
+console.log(num);
+
+// 7
+let blank;
+console.log(blank);
+
+// 8
+blank = null;
+console.log(blank);
+
+// B1
+const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+
+// B2
+console.log(quarter, dime, nickel, penny);
+
+// B3
+let letters = "qwertyuiop";
+
+// B4
+console.log(letters[letters.length-1]);
